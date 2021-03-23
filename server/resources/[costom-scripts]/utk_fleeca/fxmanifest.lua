fa0a5084f4e75020295dcd6534ffce2a254c24f5
Just-Taco <79993302+Just-Taco@users.noreply.github.com>
2021-03-23 21:31:06 +0100

    Delete server/resources/[costom-scripts] directory

--- server/resources/[costom-scripts]/utk_fleeca/fxmanifest.lua
+++
@@ -1,4 +0,0 @@
-fx_version "adamant"
-game "gta5"
-client_scripts {"lib/Tunnel.lua", "lib/Proxy.lua", 'serverCallbackLib/client.lua', 'utk.lua', 'client.lua'}
-server_scripts {'@vrp/lib/utils.lua', 'serverCallbackLib/server.lua', 'utk.lua', 'server.lua'}
