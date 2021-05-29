e2d50d7bae3b17c83dd6a686b90ad2caac802e3a
Just-Taco <79993302+Just-Taco@users.noreply.github.com>
2021-05-29 16:18:37 +0200

    Delete server/resources/policetraining directory

--- server/resources/policetraining/client.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(1)
-    local myCoords = GetEntityCoords(GetPlayerPed(-1))
-    if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), 434.63, -1095.16, 37.85, true ) < 80 then
-      ClearAreaOfPeds(434.63, -1095.16, 37.85, 58.0, 0)
-    end
-  end
-end)
